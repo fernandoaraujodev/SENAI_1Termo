d650c159dd0ed6c4341a9f797c969411cd76eaa1
Fernando Araujo <fernandovcaraujo@gmail.com>
2020-08-17 17:48:51 -0300

    Primeiro POO

---
+++ PrimeiroPOO/Automovel.cs
@@ -0,0 +1,23 @@
+namespace PrimeiroPOO
+{
+    public class Automovel 
+    {
+
+        public string modelo = "HB20";
+
+        public string marca = "Hyundai";
+
+        public string motor;
+
+    public string Andar()
+    {
+        return "O automovel andou!";
+    }
+    public string Frear()
+    {
+        return "O automovel freou!";
+    }
+
+
+    }
+}
---
+++ PrimeiroPOO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrimeiroPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Automovel HB20 = new Automovel();
+            HB20.motor = "V8 turbo";
+        
+            Console.WriteLine(HB20.modelo);
+            
+            Console.WriteLine(HB20.motor);
+            
+            Console.WriteLine(HB20.Andar());
+        
+            Console.WriteLine(HB20.Frear());
+        }
+    }
+}
